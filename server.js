d5f73a8d185a7873733842116869ec95b4080212
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-26 03:38:55 +0100

    setting up

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const router = require("./routes/home");
+
 const app = express();
 
+app.use(express.static("public"));
 app.use("/tobisam", router);
-
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("server is running of port 3000...");
+});
