83f3bf423a20acb238594a8bfd7b6df211eff1fa
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-02 03:46:28 +0100

    setting up

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,27 @@ function mouseMove(e) {
   cursor.style.cssText = cursor2.style.cssText =
     "left: " + e.clientX + "px; top: " + e.clientY + "px";
 }
+
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".wrapper");
+
+let load = 0;
+
+let int = setInterval(blurring, 60);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
