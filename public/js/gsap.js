04bd379178858cfc0bde0b868f26c0d8d7f448df
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-23 17:46:50 +0100

    working on sections

--- public/js/gsap.js
+++ public/js/gsap.js
@@ -176,3 +176,21 @@ t1.fromTo(
   },
   3.5
 );
+
+TweenMax.to(".first", 1.5, {
+  delay: 1,
+  left: "-100%",
+  ease: Expo.easeInOut
+});
+
+TweenMax.to(".second", 1.5, {
+  delay: 1.5,
+  left: "-100%",
+  ease: Expo.easeInOut
+});
+
+TweenMax.to(".third", 1.5, {
+  delay: 2.0,
+  left: "-100%",
+  ease: Expo.easeInOut
+});
