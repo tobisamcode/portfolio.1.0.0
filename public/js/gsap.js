94129885f08e75f2def1b2795a1ef8e7ef7446b1
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-07-23 15:24:12 +0100

    working on sections

--- public/js/gsap.js
+++ public/js/gsap.js
@@ -1,3 +1,7 @@
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+// let smoother = scrollSmoother.create();
+
 const t1 = new TimelineMax();
 
 t1.fromTo(".circle-outer", 1, { opacity: "0" }, { opacity: "1" }, 8);
