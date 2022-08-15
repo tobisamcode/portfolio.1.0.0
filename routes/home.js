85293548341a764a160e4503f2863808dc75495a
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-15 22:30:55 +0100

    adding new featrures

--- routes/home.js
+++ routes/home.js
@@ -7,12 +7,24 @@ router.get("/", (req, res) => {
   res.render("home", { path: "/", pageTitle: "Portfolio " });
 });
 
-router.get("/main", (req, res) => {
-  res.render("main", { path: "/main", pageTitle: "Tobi" });
+router.get("/home", (req, res) => {
+  res.render("main", { path: "/home", pageTitle: "Tobi" });
 });
 
 router.get("/about", (req, res) => {
   res.render("about", { path: "/about", pageTitle: "About me" });
+});
+
+router.get("/projects", (req, res) => {
+  res.render("projects", { path: "/projects", pageTitle: "My projects" });
+});
+
+router.get("/blogs", (req, res) => {
+  res.render("blogs", { path: "/blogs", pageTitle: "My writings" });
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact", { path: "/contact", pageTitle: "Contact me" });
 });
 
 module.exports = router;
